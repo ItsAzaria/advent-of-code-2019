6adee25c0298f9b77a6fed4e597f79c8a3c756af
moe <moe9999@protonmail.com>
2019-12-01 22:31:32 +0000

    Refactor part 1 and implement part 2 of day 1

--- the_tyranny_of_the_rocket_equation/bin/main.dart
+++ the_tyranny_of_the_rocket_equation/bin/main.dart
@@ -1,16 +1,31 @@
 import 'dart:io';
 
-main() async {
+int totalFuelRequirements = 0;
+int totalFuelRequirementsPt2 = 0;
 
-  int totalFuelRequirements = 0;
-  
+main() async {
   List<String> lines = await new File('bin/numbers.txt').readAsLines();
   List<int> modules = lines.map((line) => int.parse(line)).toList();
 
   modules.forEach((module){ 
-    totalFuelRequirements += (module / 3).floor() - 2;
+    part1(module);
+    part2(module);
   });
 
-
-  print(totalFuelRequirements);
+  print('Part 1: $totalFuelRequirements');
+  print('Part 2: $totalFuelRequirementsPt2');
 }
+
+part1(int mass) {
+  totalFuelRequirements += calcFuel(mass);
+}
+
+part2(int mass) {
+  int fuel = calcFuel(mass);
+  if (fuel <= 0) { return; }
+  
+  totalFuelRequirementsPt2 += fuel;
+  part2(fuel);
+}
+
+int calcFuel(int mass) { return (mass / 3).floor() - 2; }
